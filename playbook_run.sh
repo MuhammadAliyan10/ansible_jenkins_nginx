40759bb881c39881231c1aa17c6a91af7f08fbc9
Muhammad Aliyan <135156764+MuhammadAliyan10@users.noreply.github.com>
2023-11-01 14:00:55 +0500

    Update playbook_run.sh

--- playbook_run.sh
+++ playbook_run.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 
-ansible-playbook nginx_playbook.yml  --private-key=/home/ubuntu/keys/jenkins_ansible.pem
+ansible-playbook -i inventory.ini -e configure.ini nginx_playbook.yml --private-key=/home/ubuntu/keys/jenkins_ansible.pem
