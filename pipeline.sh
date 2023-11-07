303331e251f6a353e3fa99f2e7ed81f588ff7ba0
MuhammadAliyan10 <135156764+MuhammadAliyan10@users.noreply.github.com>
2023-11-07 11:57:17 +0500

    Updated

--- pipeline.sh
+++ pipeline.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Jenkins job URL to trigger the build
-JENKINS_JOB_URL="http://localhost:8080/job/ansible_nginx/build"
+job_url="http://localhost:8080/job/ansible_nginx/build"
 
-# Use the Jenkins API token for authentication
-JENKINS_API_TOKEN="110acf895be909cc0115c4f6ec011843ee"
+api_token="110acf895be909cc0115c4f6ec011843ee"
 
-# Make the API call to trigger the build
-curl -X POST $JENKINS_JOB_URL --user "aliyan:$JENKINS_API_TOKEN"
+
+curl -X POST $job_url --user "aliyan:$api_token"
